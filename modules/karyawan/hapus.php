fbb13dfb192881e9e0011b7454408dec27ff2ba9
Aidin <andiaidinakbar.com>
2026-03-08 14:14:11 +0800

    fix error hapus user

--- modules/karyawan/hapus.php
+++ modules/karyawan/hapus.php
@@ -6,6 +6,7 @@ require '../../config/database.php';
 allow_roles(['admin']);
 
 if (isset($_GET['id'])) {
+
     $id = mysqli_real_escape_string($conn, $_GET['id']);
     $current_admin_id = $_SESSION['id_user'];
 
@@ -15,8 +16,8 @@ if (isset($_GET['id'])) {
         exit;
     }
 
-    // 2. CEK DATA TARGET & PROTEKSI HIERARKI
-    $check_target = mysqli_query($conn, "SELECT tanda_tangan, level_akses FROM users WHERE id_user = '$id'");
+    // 2. CEK DATA TARGET
+    $check_target = mysqli_query($conn, "SELECT tanda_tangan, level_akses FROM users WHERE id_user='$id'");
     $data_karyawan = mysqli_fetch_assoc($check_target);
 
     if (!$data_karyawan) {
@@ -24,26 +25,49 @@ if (isset($_GET['id'])) {
         exit;
     }
 
-    // Jika yang login level 'staff' tapi mencoba menghapus level 'admin'
-    if ($_SESSION['level_akses'] !== 'Admin' && $data_karyawan['level_akses'] === 'admin') {
+    // 3. PROTEKSI HIERARKI
+    if ($_SESSION['level_akses'] !== 'admin' && $data_karyawan['level_akses'] === 'admin') {
         header("Location: index.php?status=forbidden");
         exit;
     }
 
-    // 3. JALANKAN QUERY HAPUS
-    $query_delete = "DELETE FROM users WHERE id_user = '$id'";
-    
-    if (mysqli_query($conn, $query_delete)) {
-        // Hapus file tanda tangan jika bukan default
+    // 4. MULAI TRANSACTION
+    mysqli_begin_transaction($conn);
+
+    try {
+
+        // Hapus semua data cuti milik user
+        mysqli_query($conn, "DELETE FROM pengajuan_cuti WHERE id_user='$id'");
+
+        // Hapus user
+        mysqli_query($conn, "DELETE FROM users WHERE id_user='$id'");
+
+        // Commit jika semua berhasil
+        mysqli_commit($conn);
+
+        // 5. Hapus file tanda tangan jika bukan default
         if ($data_karyawan['tanda_tangan'] != 'default_ttd.png') {
             $path = "../../assets/img/ttd/" . $data_karyawan['tanda_tangan'];
-            if (file_exists($path)) { unlink($path); }
+            if (file_exists($path)) {
+                unlink($path);
+            }
         }
+
         header("Location: index.php?status=success");
-    } else {
+        exit;
+
+    } catch (Exception $e) {
+
+        // Jika error rollback
+        mysqli_rollback($conn);
+
         header("Location: index.php?status=error");
+        exit;
     }
+
 } else {
+
     header("Location: index.php");
+    exit;
 }
-exit;
+?>
